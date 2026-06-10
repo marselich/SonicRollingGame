d46bac2c34d4ce9839307bc916b4833e98feec5d
Marsell <70841587+marselich@users.noreply.github.com>
2026-06-11 00:06:12 +0500

    Added environment

--- Assets/_Game/Scripts/PlayerMovement.cs
+++ Assets/_Game/Scripts/PlayerMovement.cs
@@ -9,25 +9,28 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private float _moveSpeed = 30f;
     [SerializeField] private float _rotationSpeed = 100;
+    [SerializeField] private float _jumpForce = 10;
     [SerializeField] private Animator _animator;
 
     private bool _isMoving;
+    private bool _isJump;
+    private bool _isOnGround;
 
     public PhysicsMovement PhysicsMovement { get; private set; }
 
     private void Awake()
     {
         _isMoving = false;
+        _isOnGround = true;
         Rigidbody rigidbody = GetComponent<Rigidbody>();
-        PhysicsMovement = new PhysicsMovement(rigidbody, _moveSpeed, _rotationSpeed);
+
+        PhysicsMovement = new PhysicsMovement(rigidbody, _moveSpeed, _rotationSpeed, _jumpForce);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            PhysicsMovement.Jump();
-        }
+        if (Input.GetKeyDown(KeyCode.Space) && _isOnGround)
+            _isJump = true;
 
         PhysicsMovement.Direction = new Vector3(Input.GetAxisRaw(HorizontalAxisName), 0, Input.GetAxisRaw(VerticalAxisName));
 
@@ -50,7 +53,24 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (_isJump)
+        {
+            PhysicsMovement.Jump();
+            _isJump = false;
+        }
+
         if (_isMoving)
             PhysicsMovement.ForceMove();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision != null)
+            _isOnGround = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        _isOnGround = false;
     }
 }
